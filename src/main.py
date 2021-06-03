4ea343640779467315448a8bc03d9ef625fb7420
Nikita Lazutin <74084692+ALLterago@users.noreply.github.com>
2021-06-03 23:33:57 +1000

    рандом фичи(потом сделаю)

--- src/main.py
+++ src/main.py
@@ -3,18 +3,18 @@ print("\nAvailable commands:")
 print("devTest - testing/debug\nTKinter - GUI stuff\n")
 
 
-swcompl = int(0)
+swcompl = int(0) 
 
 while swcompl == 0 :
-    startcommand = input()
+    startcommand = input()           
     if startcommand == "devTest":
-        print("Launching " + startcommand)
+        print("Launching " + startcommand)   #всякая дебуг шляпа и тест фич перед внедрением
         swcompl=int(1)
     elif startcommand == "iddqd":
-        print("Launching " + startcommand)
+        print("Launching " + startcommand)   #тестю запуск кода из внешних файлов. ссылка на ориг: https://github.com/Pink-Silver/PyDoom
         swcompl=int(1)
     elif startcommand == "TKinter":
-        print("Launching " + startcommand)
+        print("Launching " + startcommand)   #тестю ткинтер
         swcompl=int(1)
     else:
         print("Wrong command")
