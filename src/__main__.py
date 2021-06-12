7fa8896419552ab4f9fad44a954ec79f256fa57e
Nikita Lazutin <74084692+ALLterago@users.noreply.github.com>
2021-06-12 12:52:50 +1000

    some strange stuff

---
+++ src/__main__.py
@@ -0,0 +1,36 @@
+from devTest import SumInt
+
+
+
+Title = "Akemi\nROFL\nMaker\nPython\nEdition"
+TitleLine = "_______________________________________________________________________"
+print(TitleLine)
+print(Title) #enter block
+print("\nAvailable commands:")
+print("devTest - testing/debug\nTKinter - GUI stuff\nexit - guess what it does by yourself\n")
+
+swcompl = int(0) 
+
+while swcompl == 0 :
+    startcommand = input()           
+    if startcommand == "devTest":
+        print("Launching " + startcommand)   #всякая дебуг шляпа и тест фич перед внедрением
+        print(SumInt(int(input("Input first int number:")),int(input("Input second int number:"))))
+        swcompl=int(1)
+
+    elif startcommand == "iddqd":
+        print("Launching " + startcommand)   #тестю запуск кода из внешних файлов. ссылка на ориг: https://github.com/Pink-Silver/PyDoom
+        swcompl=int(1)
+
+    elif startcommand == "TKinter":
+        print("Launching " + startcommand)   #тестю ткинтер
+
+        swcompl=int(1)
+
+    elif startcommand == "exit":
+        print("Bye...")
+        swcompl=int(1)
+        
+    else:
+        print("Wrong command")
+
---
+++ src/devTest.py
@@ -0,0 +1,4 @@
+def SumInt(x,y):   #тестю методы 1
+    return x+y
+
+
