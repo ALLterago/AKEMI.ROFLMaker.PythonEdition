e73105cb68321291270e1b6162a249f265c7e44d
ALLterago <russianrarity@gmail.com>
2021-11-28 11:03:17 +1000

    added hacking rofl util

--- commands.py
+++ commands.py
@@ -1,15 +1,59 @@
+import time
+
+from random import seed
+from random import randint
+from rich.progress import Progress,track
+
+command_list = {
+'0':'exit',
+'1':'hacking rofl',
+'2':'steam idler'}
+
 def comExectutor(comid):
-	if comid == 1:
-		print(comid)
-		return 0
+
+	if comid == 0:
+		print("Bye...")
+		return True
+
+	elif comid == 1:
+		hackingRofl()
+		print("Hack compete")
+		return False
 
 	elif comid == 2:
 		print(comid)
-		return 0
-
-	elif comid == 0:
-		return 1
+		return False
 
 	else:
 		print("Command id unknown")
-		return 0 
+		return False
+
+
+def hackingRofl():
+	for i in track(range(50),"Initializing hacking utils..."):
+		time.sleep(0.1)
+		
+	hackTarget = str(input("Hack target:"))
+	
+	for i in track(range(100),"identifying " + hackTarget):
+		time.sleep(0.1)
+		
+	for i in track(range(125),"Configurating IP hack"):
+		time.sleep(0.1)
+
+	seed(hackTarget)
+	with Progress(transient=True) as prog :
+		iphack = prog.add_task("Hacking bank accounts",total=randint(500, 1500))
+		jopahack = prog.add_task("Hacking jopa",total=randint(750,1750))
+		vkhack = prog.add_task("Hacking Vkontakte",total=randint(250,1250))
+		while not prog.finished:
+			prog.update(iphack,advance=0.4)
+			prog.update(jopahack,advance=0.6)
+			prog.update(vkhack,advance=0.2)
+			time.sleep(0.02)
+
+	for i in track(range(50),"Searching for address"):
+		time.sleep(0.1)
+
+	for i in track(range(100),"Sending dagestan fighters"):
+		time.sleep(0.1)
--- roflmaker.py
+++ roflmaker.py
@@ -4,11 +4,6 @@ import commands
 
 from rich.console import Console
 
-command_list = {
-'0':'exit',
-'1':'hacking rofl',
-'2':'steam idler'}
-
 def showBanner(banner_text):
 	banner = pyfiglet.figlet_format(banner_text,font="banner3-D")
 	print(banner)
@@ -16,16 +11,24 @@ def showBanner(banner_text):
 
 def showCommandList():
 	print("Available commands:")
-	for i in range(len(command_list)):
-		print(str(i) + ":" + command_list[str(i)] + "\n")
+	for i in range(len(commands.command_list)):
+		print(str(i) + ":" + commands.command_list[str(i)] + "\n")
 
 
 def main():
+	isOver = False
+	
 	showBanner("akemi rofl maker")
 	showCommandList()
 
-	while commands.comExectutor(int(input("Enter command id:"))) != 1:
-		pass
+	while isOver != True:
+		
+		try:
+			isOver = commands.comExectutor(int(input("Enter command id:")))
+		except ValueError:
+			print("Invalid id input type")
+			
+		showCommandList()
 
 
 if __name__ == "__main__":
