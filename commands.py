420cd5496a4de0d0dce86ddb22914e80199c1d26
ALLterago <russianrarity@gmail.com>
2021-11-28 20:35:10 +1000

    time counter for walk util

--- commands.py
+++ commands.py
@@ -5,30 +5,12 @@ from random import seed
 from random import randint
 from rich.progress import Progress,track
 
+
 command_list = {
 '0':'exit',
 '1':'hacking rofl',
 '2':'walk cool looking console util',
 '3':'steam idler'}
-
-def comExectutor(comid):
-
-	if comid == 0:
-		print("Bye...")
-		return True
-
-	elif comid == 1:
-		hackingRofl()
-		print("Hack compete")
-		return False
-
-	elif comid == 2:
-		walkUtil()
-		return False
-
-	else:
-		print("Command id unknown")
-		return False
 
 
 def hackingRofl():
@@ -37,14 +19,14 @@ def hackingRofl():
 		time.sleep(0.1)
 		
 	hackTarget = str(input("Hack target:"))
+	seed(hackTarget)
 	
 	for i in track(range(100),"identifying " + hackTarget):
 		time.sleep(0.1)
 		
 	for i in track(range(125),"Configurating IP hack"):
 		time.sleep(0.1)
-
-	seed(hackTarget)
+		
 	with Progress(transient=True) as prog :
 		iphack = prog.add_task("Hacking bank accounts",total=randint(500, 1500))
 		jopahack = prog.add_task("Hacking jopa",total=randint(750,1750))
@@ -62,8 +44,46 @@ def hackingRofl():
 		time.sleep(0.1)
 
 
-def walkUtil():
+def walkUtil(walkTime):
 
+	walkTimePass = 0
+
+	
 	for i in walk("/"):
 		print(i)
 		time.sleep(0.3)
+		walkTimePass += 0.3
+
+		if walkTimePass > walkTime:
+			break
+
+	return True
+
+
+def comExectutor(comid):
+
+	if comid == 0:
+		print("Bye...")
+		return True
+
+	elif comid == 1:
+		hackingRofl()
+		print("Hack compete")
+		return False
+
+	elif comid == 2:
+		isComplete = False
+		
+		while isComplete != True:
+			try:
+				isComplete = walkUtil(int(input("Walk time in seconds:")))
+			except Exception:
+				print("Wrong time format")
+		return False
+
+	else:
+		print("Command id unknown")
+		return False
+
+
+
